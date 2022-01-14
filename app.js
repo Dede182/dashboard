9fab9e591be146089531f6d25bc62713380e51bd
Dede182 <79628155+Dede182@users.noreply.github.com>
2022-01-14 23:41:36 +0630

    Update app.js

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ let progressbar = document.querySelectorAll(".circular-progress")
 let cirvalue = document.querySelectorAll(".cirvalue")
 console.log(progressbar);
 let speed = 10;
-
-for(let i=0;i<cirvalue.length;i++){
+function p(){
+    for(let i=0;i<cirvalue.length;i++){
     console.log(cirvalue[i].innerText)
     let progressendvalue = cirvalue[i].innerText; 
     let progressvalue = 0;  
@@ -22,8 +22,58 @@ for(let i=0;i<cirvalue.length;i++){
         
         }
     ,speed)
+    
+}}
+p();
+let light = document.getElementById("lightmode");
+const dede = document.querySelectorAll(".sales");
+const meme = document.querySelectorAll(".item-online");
+console.log(dede)
+let a = "light_mode";
+let b = "dark_mode";
+light.addEventListener('click',function rew(){
+    document.querySelector('aside').classList.toggle('font-dark-theme');
+    document.querySelector('main').classList.toggle('font-dark-theme');
+    document.querySelector('.recent-order').classList.toggle('font-dark-theme');
+    document.querySelector('.recent-table').classList.toggle('box-dark');
+    document.querySelector('.update-box').classList.toggle('box-dark');
+    document.querySelector('.container').classList.toggle('dark-theme-variables');
+    document.querySelector(".switch").classList.toggle('lightwhite')
 
-}
+    for(let i =0;i<dede.length;i++){
+        dede[i].classList.toggle('box-dark')
+    }
+    for(let i =0;i<dede.length;i++){
+        meme[i].classList.toggle('box-dark')
+    }
+    function one(){
+       if(light.innerText = a){
+           light.innerText = b;
+
+        light.addEventListener('click',()=>{
+            light.innerText = a;
+            light.addEventListener('click',()=>{
+                one();
+            })
+        })
+        
+       } 
+    }
+    one();
+   
+       
+       
+     }
+    
+)
+// light.innerText = b;
+// if(light.innerText = b){
+//     light.addEventListener('click',()=>{
+//       light.innerText = a;  
+//     }
+//     )
+// }
+
 
 
 
